13725d305568e47eb94b37d6c3dcb3d3f81099b6
HotPotatoes42 <iarislav.buharev@gmail.com>
2026-09-12 11:23:58 +0300

    Step 7: variables and data types

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,4 +1,66 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Привет от ИСП-242!");
-Console.WriteLine("Автор: Бухарев Ярослав");
-Console.WriteLine("Год: 2026");
+// Console.WriteLine("Привет от ИСП-242!");
+// Console.WriteLine("Автор: Бухарев Ярослав");
+// Console.WriteLine("Год: 2026");
+
+// int age = 20;
+// double gpa = 4.5;
+// bool isStudent = true;
+// string name = "Ярослав";
+// Console.WriteLine(name);
+// Console.WriteLine(age);
+// Console.WriteLine(gpa);
+// Console.WriteLine(isStudent);
+
+// var city = "Москва";
+// var year = 2026;
+// var pi = 3.14159;
+// var isActive = true;
+// Console.WriteLine(city);
+// Console.WriteLine(year);
+// Console.WriteLine(pi);
+// Console.WriteLine(isActive);
+
+// string myName = "Ярослав";
+// int myAge = 18;
+// string myGroup = "ИСП-242";
+// Console.WriteLine($"Меня зовут {myName}, мне {myAge} лет, я учусь в группе {myGroup}.");
+
+// Console.Write("Введите ваше имя: ");
+// string name2 = Console.ReadLine();
+// Console.WriteLine($"Привет, {name2}!");
+
+// Console.Write("Введите ваш возраст");
+// string input = Console.ReadLine();
+// int age2 = int.Parse(input);
+// Console.WriteLine($"Через 10 лет вам будет {age + 10} лет.");
+
+// int x = 10;
+// int y = 3;
+// Console.WriteLine(x + y);
+// Console.WriteLine(x - y);
+// Console.WriteLine(x * y);
+// Console.WriteLine(x / y);
+// Console.WriteLine(x % y);
+
+string firstName = "Ярослав";
+string lastName = "Бухарев";
+string group = "ИСП-242";
+int birthYear = 2008;
+double gpa = 4.5;
+bool hasScholarship = false;
+
+int currentYear = 2026;
+int age = currentYear - birthYear;
+
+Console.WriteLine("Студенческое удостоверение");
+Console.WriteLine($"Имя: {firstName} {lastName}");
+Console.WriteLine($"Группа: {group}");
+Console.WriteLine($"Возраст: {group}");
+Console.WriteLine($"Средний балл: {age}");
+Console.WriteLine($"Стипендия: {hasScholarship}");
+
+Console.WriteLine("\nВведите ваш любимый предмет: ");
+string subject = Console.ReadLine();
+Console.WriteLine($"Отлично! {firstName} любит {subject}");
+
